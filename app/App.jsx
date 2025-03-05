5dbf12be32083a5165cc09e30ec807cb669c0a29
ayushsharma-1 <ayushsharma18001@gmail.com>
2025-03-05 14:12:59 +0530

    Changing BrowerRouter to hashrouter

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from "../src/jsx/AboutPage/about.jsx";
 import Faqs from '../src/jsx/CommunityPages/faqs';
 import News from '../src/jsx/NewsPage/news';
