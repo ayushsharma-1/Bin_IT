a1025a2292ec373acd0185ac84774975f1e81414
Ashmit <ashmitkumar1020@gmail.com>
2025-03-05 14:27:03 +0530

    Update routes

--- app/App.jsx
+++ app/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Route, Routes, Navigate } from 'react-router-dom';
 import About from "../src/jsx/AboutPage/about.jsx";
 import Faqs from '../src/jsx/CommunityPages/faqs';
 import News from '../src/jsx/NewsPage/news';
 import Home from '../src/jsx/HomePage/home';
+// import NotFound from '../src/jsx/Commen-Components/NotFound.jsx';
 import ReportPage from '../src/jsx/ReportPage/report.jsx';
 import Ngo from '../src/jsx/RegistrationPages/ngo.jsx';
 
+
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/faqs" element={<Faqs />} />
-                <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
-                <Route path="/ngo-registration" element={<Ngo />} />
-                <Route path="*" element={<Home />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/faqs" element={<Faqs />} />
+            <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
+            <Route path="/ngo-registration" element={<Ngo />} />
+            <Route path="*" element={<Home />} />
+        </Routes>
     );
 }
-
 export default App;
