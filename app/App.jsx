a3a4b0a25cc1cf86524ace4e5522bcfa2e30090f
Rudra Singh <2k22.cse.2213360@gmail.com>
2025-04-01 06:44:26 +0530

    chatbot

--- app/App.jsx
+++ app/App.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import {Route, Routes, Navigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import About from "../src/jsx/AboutPage/about.jsx";
 import Faqs from '../src/jsx/CommunityPages/faqs';
 import News from '../src/jsx/NewsPage/news';
 import Home from '../src/jsx/HomePage/home';
 import ReportPage from '../src/jsx/ReportPage/report.jsx';
 import Ngo from '../src/jsx/RegistrationPages/ngo.jsx';
+import ChatbitEmbed from '../src/jsx/chatbot.jsx';
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <div className="spinner"></div>
+        </div>
+    );
+};
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for demonstration purposes
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Change 2000ms to document.readyState check if needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/faqs" element={<Faqs />} />
-            <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
-            <Route path="/ngo-registration" element={<Ngo />} />
-            <Route path="*" element={<Home />} />
-        </Routes>
+        <>
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                <ChatbitEmbed />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/news" element={<News />} />
+                    <Route path="/faqs" element={<Faqs />} />
+                    <Route path="/submit-report" element={<ReportPage totalPages={4} />} />
+                    <Route path="/ngo-registration" element={<Ngo />} />
+                    <Route path="*" element={<Home />} />
+                </Routes>
+                </>
+            )}
+        </>
     );
 }
 export default App;
